6318e5a565c90da6fbe347c57b653761b6b69305
Kashish Kohli <kashish.kohli56@gmail.com>
2018-10-09 19:26:18 +0530

    Created Header section

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Decision Maker</h1>
+      <p>Let us decide what's best for you to do right now.</p>
+    </div>
+  );
+};
+
+export default Header;
